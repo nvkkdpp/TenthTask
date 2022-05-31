9de78692226713006dd53a69f562190eadce24ea
nvkkdpp <niggamemes@ya.ru>
2022-05-31 23:53:18 +0900

    commit

---
+++ TenthTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TenthTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] chisla = {5, 2, 7, 3.5, 6.5, 8, 1, 9, 1, 3, 1, 2,4};
+            double sum = 0;
+            for (int i = 0; i < 13; i++)
+            {
+                sum = sum + chisla[i];
+            }
+            double vsyovmeste = sum / 10;
+            Console.WriteLine(vsyovmeste);
+            Console.WriteLine("нажмите <enter> для выхода . . . ");
+            Console.Read();
+        }
+    }
+}
